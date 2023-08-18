3d42eed62bcd8b04499acc22c312cdec5723dabe
David <david.niemann@hs-furtwangen.de>
2023-08-18 13:30:21 +0200

    add: modinfo

---
+++ modinfo.lua
@@ -0,0 +1,13 @@
+-- This information tells other players more about the mod
+name = "unfriendly scarecrow"
+description = "mini boss"
+author = "Bambusbjörn and Sanoggel"
+version = "0.0.1"
+
+all_clients_require_mod = true
+client_only_mod = true
+dont_starve_compatible = true
+reign_of_giants_compatible = true
+shipwrecked_compatible = true
+dst_compatible = true
+api_version = 10
