48c0eab544ceb6f95b4ee70f402538580a16b664
David <david.niemann@hs-furtwangen.de>
2023-08-18 13:49:02 +0200

    update: modinfo

--- modinfo.lua
+++ modinfo.lua
@@ -4,10 +4,17 @@ description = "mini boss"
 author = "Bambusbjörn and Sanoggel"
 version = "0.0.1"
 
-all_clients_require_mod = true
-client_only_mod = true
-dont_starve_compatible = true
-reign_of_giants_compatible = true
-shipwrecked_compatible = true
-dst_compatible = true
+
+-- This lets other players know if your mod is out of date, update it to match the current version in the game
 api_version = 10
+
+-- Compatible with Don't Starve Together
+dst_compatible = true
+
+-- Not compatible with Don't Starve
+dont_starve_compatible = false
+reign_of_giants_compatible = false
+shipwrecked_compatible = false
+
+-- Character mods are required by all clients
+all_clients_require_mod = true 
