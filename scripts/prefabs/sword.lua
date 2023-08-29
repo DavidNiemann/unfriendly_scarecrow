483fda5a43f13bb496922f3ca815c12c9a7a7970
David <david.niemann@hs-furtwangen.de>
2023-08-29 16:22:27 +0200

    add: best onhit effect

--- scripts/prefabs/sword.lua
+++ scripts/prefabs/sword.lua
@@ -1,61 +1,64 @@
+local spawn_chance = 1
 local assets =
 {
-	-- Animation files used for the item.
-	Asset("ANIM", "anim/sword.zip"),
+    -- Animation files used for the item.
+    Asset("ANIM", "anim/sword.zip"),
 
-	-- Inventory image and atlas file used for the item.
+    -- Inventory image and atlas file used for the item.
     Asset("ATLAS", "images/inventoryimages/sword.xml"),
     Asset("IMAGE", "images/inventoryimages/sword.tex"),
 }
 prefabs = {
     "sword",
 }
-local function onequip(inst, owner) 
+
+
+local function onequip(inst, owner)
     owner.AnimState:OverrideSymbol("swap_object", -- Symbol to override.
-    	"sword", -- Animation bank we will use to overwrite the symbol.
-    	"sword") -- Symbol to overwrite it with.
-    owner.AnimState:Show("ARM_carry") 
-    owner.AnimState:Hide("ARM_normal") 
+        "sword",                                  -- Animation bank we will use to overwrite the symbol.
+        "sword")                                  -- Symbol to overwrite it with.
+    owner.AnimState:Show("ARM_carry")
+    owner.AnimState:Hide("ARM_normal")
 end
 
-local function onunequip(inst, owner) 
-    owner.AnimState:Hide("ARM_carry") 
-    owner.AnimState:Show("ARM_normal") 
+local function onunequip(inst, owner)
+    owner.AnimState:Hide("ARM_carry")
+    owner.AnimState:Show("ARM_normal")
 end
 
-local function fireattack(inst, attacker, target)
-    if not target:IsValid() then
-        --target killed or removed in combat damage phase
-        return
-    end
---[[ 
-	if target.SoundEmitter ~= nil then
-	    target.SoundEmitter:PlaySound("dontstarve/wilson/blowdart_impact_fire")
-	end ]]
+local function NoHoles(pt)
+    return not TheWorld.Map:IsPointNearHole(pt)
+end
 
-    target:PushEvent("attacked", {attacker = attacker, damage = 0})
-    -- NOTES(JBK): Valid check in case the event removed the target.
-    if target:IsValid() then
-        if target.components.burnable then
-            target.components.burnable:Ignite(nil, attacker)
+local function onattack(inst, owner, target)
+    if math.random() < spawn_chance then
+        local pt
+        if target ~= nil and target:IsValid() then
+            pt = target:GetPosition()
+        else
+            pt = owner:GetPosition()
+            target = nil
         end
-        if target.components.freezable then
-            target.components.freezable:Unfreeze()
-        end
-        if target.components.health then
-            target.components.health:DoFireDamage(0, attacker)
-        end
-        if target.components.combat then
-            target.components.combat:SuggestTarget(attacker)
+        local offset = FindWalkableOffset(pt, math.random() * 2 * PI, 2, 3, false, true, NoHoles, false, true)
+        if offset ~= nil then
+           --[[  inst.SoundEmitter:PlaySound("dontstarve/common/shadowTentacleAttack_1")
+            inst.SoundEmitter:PlaySound("dontstarve/common/shadowTentacleAttack_2") ]]
+            for i = 1, 10, 1 do
+                local tentacle = SpawnPrefab("frog")
+                if tentacle ~= nil then
+                    tentacle.Transform:SetPosition(pt.x + offset.x, 0, pt.z + offset.z)
+                    tentacle.components.combat:SetTarget(owner)
+                end
+            end
         end
     end
 end
 
 local function init()
-	local inst = CreateEntity()
+    local inst = CreateEntity()
 
-	inst.entity:AddTransform()
-	inst.entity:AddAnimState()
+    inst.entity:AddTransform()
+    inst.entity:AddAnimState()
     inst.entity:AddSoundEmitter()
     inst.entity:AddNetwork()
 
@@ -75,25 +78,25 @@ local function init()
 
     inst:AddComponent("weapon")
     inst.components.weapon:SetDamage(39)
-    inst.components.weapon:SetOnAttack(fireattack)
-    
+    inst.components.weapon:SetOnAttack(onattack)
+
     inst:AddComponent("inspectable")
-    
+
     inst:AddComponent("inventoryitem")
     inst.components.inventoryitem.atlasname = "images/inventoryimages/sword.xml"
     inst.components.inventoryitem.imagename = "sword"
-    
+
     inst:AddComponent("equippable")
     inst.components.equippable:SetOnEquip(onequip)
     inst.components.equippable:SetOnUnequip(onunequip)
-	
-	inst:AddComponent("finiteuses")
+
+    inst:AddComponent("finiteuses")
     inst.components.finiteuses:SetMaxUses(120)
     inst.components.finiteuses:SetUses(120)
-	inst.components.finiteuses:SetOnFinished(inst.Remove)
+    inst.components.finiteuses:SetOnFinished(inst.Remove)
 
     MakeHauntableLaunch(inst)
 
     return inst
 end
-return  Prefab("common/inventory/sword", init, assets, prefabs)
+return Prefab("common/inventory/sword", init, assets, prefabs)
