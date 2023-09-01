6bc19a624d23d55102602f6a8209dfeec129b9d1
SamuelKasper <Samuel.Noah.Kasper@hs-furtwangen.de>
2023-09-01 14:15:59 +0200

    frogs helping you fight now

--- scripts/brains/ravenbrain.lua
+++ scripts/brains/ravenbrain.lua
@@ -1,22 +1,8 @@
---[[ require "behaviours/follow"
-require "behaviours/wander"
-require "behaviours/doaction"
-require "behaviours/chaseandattack"
-require "behaviours/standstill"
-local BrainCommon = require("brains/braincommon") ]]
-
 require "behaviours/wander"
 require "behaviours/doaction"
 require "behaviours/chaseandattack"
 require "behaviours/standstill"
 local BrainCommon = require("brains/braincommon")
-
---[[ local MIN_FOLLOW_DIST = 2
-local TARGET_FOLLOW_DIST = 5
-local MAX_FOLLOW_DIST = 9
-
-local MAX_CHASE_TIME = 10
-local MAX_CHASE_DIST = 30 ]]
 
 local STOP_RUN_DIST = 10
 local SEE_PLAYER_DIST = 5
@@ -26,38 +12,6 @@ local SEE_TARGET_DIST = 6
 local MAX_CHASE_DIST = 7
 local MAX_CHASE_TIME = 8
 
---[[ local RavenBrain = Class(Brain, function(self, inst)
-    Brain._ctor(self, inst)
-end) ]]
-
---[[ local SHOULDFLYAWAY_MUST_TAGS = { "notarget", "INLIMBO" }
-local SHOULDFLYAWAY_CANT_TAGS = { "player", "monster", "scarytoprey" }
- ]]
---[[ local function ShouldFlyAway(inst)
-    return not (inst.sg:HasStateTag("sleeping") or
-                inst.sg:HasStateTag("busy") or
-                inst.sg:HasStateTag("flight"))
-        and (TheWorld.state.isnight or
-            (inst.components.health ~= nil and inst.components.health.takingfiredamage and not (inst.components.burnable and inst.components.burnable:IsBurning())) or
-            FindEntity(inst, inst.flyawaydistance, nil, nil, SHOULDFLYAWAY_MUST_TAGS, SHOULDFLYAWAY_CANT_TAGS) ~= nil)
-end
-
-local function FlyAway(inst)
-    inst:PushEvent("flyaway")
-end ]]
-
-local function GoHomeAction(inst)
-    if inst.components.homeseeker and
-       inst.components.homeseeker.home and
-       inst.components.homeseeker.home:IsValid() then
-        return BufferedAction(inst, inst.components.homeseeker.home, ACTIONS.GOHOME)
-    end
-end
-
-local function ShouldGoHome(inst)
-    return not TheWorld.state.isday or TheWorld.state.iswinter
-end
-
 local RavenBrain = Class(Brain, function(self, inst)
     Brain._ctor(self, inst)
 end)
@@ -65,24 +19,8 @@ end)
 function RavenBrain:OnStart()
     local root = PriorityNode(
     {
-       --[[  WhileNode( function() return self.inst.components.hauntable ~= nil and self.inst.components.hauntable.panic end, "PanicHaunted",
-        ActionNode(function() return FlyAway(self.inst) end)),
-   --[[  IfNode(function() return ShouldFlyAway(self.inst) end, "Threat Near",
-        ActionNode(function() return FlyAway(self.inst) end)), ]]
-   --[[  EventNode(self.inst, "threatnear", ]]
-   --[[      ActionNode(function() return FlyAway(self.inst) end)), ]]
---[[    WhileNode( function() return self.inst.components.combat.target == nil or not self.inst.components.combat:InCooldown() end, "AttackMomentarily",
-       ChaseAndAttack(self.inst, MAX_CHASE_TIME, MAX_CHASE_DIST) ),
-    EventNode(self.inst, "gohome",
-        ActionNode(function() return FlyAway(self.inst) end)), ]]
-
-        BrainCommon.PanicTrigger(self.inst),
+		--[[ BrainCommon.PanicTrigger(self.inst), ]]
         ChaseAndAttack(self.inst, MAX_CHASE_TIME),
-        WhileNode(function() return ShouldGoHome(self.inst) end, "ShouldGoHome",
-            DoAction(self.inst, function() return GoHomeAction(self.inst) end, "go home", true )),
-		WhileNode(function() return TheWorld and not TheWorld.state.isnight end, "IsNotNight",
-			Wander(self.inst, function() return self.inst.components.knownlocations:GetLocation("home") end, MAX_WANDER_DIST)),
-		StandStill(self.inst, function() return self.inst.sg:HasStateTag("idle") end, nil),
     }, .25)
 
     self.bt = BT(self.inst, root)
--- scripts/prefabs/raven.lua
+++ scripts/prefabs/raven.lua
@@ -1,88 +1,43 @@
---[[
-birds.lua
-This assumes the bird already has a build, inventory icon, sounds and a feather_name prefab exists
-]]--
 local brain = require "brains/RavenBrain"
 
-local function ShouldSleep(inst)
-    return DefaultSleepTest(inst) and not inst.sg:HasStateTag("flying")
-end
-
---[[ local function OnAttacked(inst, data)
-    local x,y,z = inst.Transform:GetWorldPosition()
-    local ents = TheSim:FindEntities(x,y,z, 30, {'bird'})
-    
-    local num_friends = 0
-    local maxnum = 5
-    for k,v in pairs(ents) do
-        if v ~= inst then
-            v:PushEvent("gohome")
-            num_friends = num_friends + 1
-        end
-        
-        if num_friends > maxnum then
-            
-			return
-        end
-        
-    end
-end ]]
-
+-- if bird is attacked
 local function OnAttacked(inst, data)
     inst.components.combat:SetTarget(data.attacker)
-   --[[  inst.components.combat:ShareTarget(data.attacker, 30, function(dude) return dude:HasTag("frog") and not dude.components.health:IsDead() end, 5) ]]
+    inst.components.combat:ShareTarget(data.attacker, 30,
+        function(dude) return dude:HasTag("frog") and not dude.components.health:IsDead() end, 5)
 end
-
-local function OnTrapped(inst, data)
-
-    if data and data.trapper and data.trapper.settrapsymbols then
-        data.trapper.settrapsymbols(inst.trappedbuild)
-    end
-end
-
-local function OnDropped(inst)
-    inst.sg:GoToState("stunned")
-end
-
 
 local function makebird(name, soundname, loottable, psprefab, foodtype, scale)
-    local assets=
+    local assets =
     {
-	    Asset("ANIM", "anim/crow.zip"),
-	    Asset("ANIM", "anim/"..name.."_build.zip"),
-	    Asset("SOUND", "sound/birds.fsb")
+        Asset("ANIM", "anim/crow.zip"),
+        Asset("ANIM", "anim/" .. name .. "_build.zip"),
+        Asset("SOUND", "sound/birds.fsb")
     }
-	
-	local prefabs = 
-	{
-		"cookedsmallmeat",
-	}
-	
-	prefabs[2] = psprefab or "seeds"-- add the periodspawnprefab to "prefabs" list
-	
-	if loottable ~= nil then-- -- add all loot to "prefabs" list
-		for key, loot in pairs(loottable) do
-			key = tonumber(key)
-			prefabs[key+2] = loot[1]--key+2 is due to the fact that "prefabs" list already has 2 values in it
-		end
-	end
-    
+
+    local prefabs =
+    {
+        "cookedsmallmeat",
+    }
+
+    prefabs[2] = psprefab or "seeds" -- add the periodspawnprefab to "prefabs" list
+
+    if loottable ~= nil then      -- -- add all loot to "prefabs" list
+        for key, loot in pairs(loottable) do
+            key = tonumber(key)
+            prefabs[key + 2] = loot[1] --key+2 is due to the fact that "prefabs" list already has 2 values in it
+        end
+    end
+
     local function fn()
         local inst = CreateEntity()
-
         --Core components
         inst.entity:AddTransform()
-
         inst.entity:AddNetwork()
-
         inst.entity:AddPhysics()
-
         inst.entity:AddAnimState()
         inst.entity:AddDynamicShadow()
-
-
         inst.entity:AddSoundEmitter()
-
 
         --Initialize physics
         inst.Physics:SetCollisionGroup(COLLISION.CHARACTERS)
@@ -91,24 +46,28 @@ local function makebird(name, soundname, loottable, psprefab, foodtype, scale)
         inst.Physics:SetSphere(1)
         inst.Physics:SetMass(1)
 
-
-        inst:AddTag("bird")
-		inst:AddTag("extrabird")
+        --Tags for inventory search??
         inst:AddTag(name)
         inst:AddTag("smallcreature")
-		
-		scale = scale or 1 
+        inst:AddTag("animal")
+        inst:AddTag("prey")
+        inst:AddTag("frog")
 
-        inst.Transform:SetTwoFaced()   
-        inst.AnimState:SetBank("crow")
-        inst.AnimState:SetBuild(name.."_build")
+        --Set default for scale
+        scale = scale or 1
+
+        inst.Transform:SetTwoFaced()
+--[[         inst.AnimState:SetBank("crow")
+        inst.AnimState:SetBuild(name .. "_build")
+        inst.AnimState:PlayAnimation("idle") ]]
+        inst.AnimState:SetBank("frog")
+        inst.AnimState:SetBuild("frog")
         inst.AnimState:PlayAnimation("idle")
-		inst.DynamicShadow:SetSize(scale, scale - 0.25)
+        inst.DynamicShadow:SetSize(scale, scale - 0.25)
         inst.DynamicShadow:Enable(false)
-		
-		inst.Transform:SetScale(scale, scale, scale)
-	
-        MakeFeedablePetPristine(inst)
+
+        inst.Transform:SetScale(scale, scale, scale)
+--[[         MakeFeedablePetPristine(inst) ]]
 
         if not TheWorld.ismastersim then
             return inst
@@ -118,102 +77,51 @@ local function makebird(name, soundname, loottable, psprefab, foodtype, scale)
 
         inst.sounds =
         {
-            takeoff = "dontstarve/birds/takeoff_"..soundname,
-            chirp = "dontstarve/birds/chirp_"..soundname,
+            takeoff = "dontstarve/birds/takeoff_" .. soundname,
+            chirp = "dontstarve/birds/chirp_" .. soundname,
             flyin = "dontstarve/birds/flyin",
         }
-        inst.trappedbuild = name.."_build"
-        
+        inst.trappedbuild = name .. "_build"
+
         inst:AddComponent("locomotor") -- locomotor must be constructed before the stategraph
         inst.components.locomotor:EnableGroundSpeedMultiplier(false)
-	    inst.components.locomotor:SetTriggersCreep(false)
-        inst:SetStateGraph("SGbird")
+        inst.components.locomotor:SetTriggersCreep(false)
+        inst:SetStateGraph("SGfrog")
 
-		if loottable ~= nil then
-			inst:AddComponent("lootdropper")
-			inst.components.lootdropper.numrandomloot = 1
-			for key, loot in pairs(loottable) do
-				inst.components.lootdropper:AddRandomLoot(loot[1], loot[2])--loot[1] is prefab, loot[2] is rarity
-			end
-		end
-		
-        inst:AddComponent("occupier")
-        
-        inst:AddComponent("eater")
-		inst.components.eater:SetDiet({ foodtype or FOODTYPE.SEEDS }, { foodtype or FOODTYPE.SEEDS })
-        
-        inst:AddComponent("sleeper")
-        inst.components.sleeper:SetSleepTest(ShouldSleep)
-
-        inst:AddComponent("inventoryitem")
-        inst.components.inventoryitem.nobounce = true
-        inst.components.inventoryitem.canbepickedup = false
-        
-        inst.components.inventoryitem.imagename = name    
-        inst.components.inventoryitem.atlasname = "images/inventoryimages/"..name..".xml"
-
-        inst:AddComponent("cookable")
-        inst.components.cookable.product = "cookedsmallmeat"
+        if loottable ~= nil then
+            inst:AddComponent("lootdropper")
+            inst.components.lootdropper.numrandomloot = 1
+            for key, loot in pairs(loottable) do
+                inst.components.lootdropper:AddRandomLoot(loot[1], loot[2]) --loot[1] is prefab, loot[2] is rarity
+            end
+        end
 
         inst:AddComponent("combat")
-        inst.components.combat.hiteffectsymbol = "crow_body"
-        --inst.components.combat.canbeattackedfn = canbeattacked
-        inst.components.combat:SetDefaultDamage(5)
-        inst.components.combat:SetAttackPeriod(5)
-        --[[ inst.components.combat:SetRetargetFunction(3, retargetfn) ]]
-    
-        inst.components.combat.onhitotherfn = OnHitOther
+        inst.components.combat:SetDefaultDamage(TUNING.FROG_DAMAGE)
+        inst.components.combat:SetAttackPeriod(TUNING.FROG_ATTACK_PERIOD)
 
         inst:AddComponent("health")
-		inst.components.health:SetMaxHealth(TUNING.BIRD_HEALTH)
-		inst.components.health.murdersound = "dontstarve/wilson/hit_animal"
-        
-        inst:AddComponent("inspectable")
-		inst.flyawaydistance = TUNING.BIRD_SEE_THREAT_DISTANCE
+        inst.components.health:SetMaxHealth(1000)
+        inst.components.health.murdersound = "dontstarve/wilson/hit_animal"
         inst:SetBrain(brain)
-        
-		--inst:AddTag("HASHEATER")
-		
+
         MakeSmallBurnableCharacter(inst, "crow_body")
         MakeTinyFreezableCharacter(inst, "crow_body")
 
-		inst:AddComponent("hauntable")
-        inst.components.hauntable:SetHauntValue(TUNING.HAUNT_TINY)
-        inst:AddComponent("periodicspawner")
-		inst.components.periodicspawner:SetPrefab(psprefab or "seeds")--set to psprefab or "seeds"(it will be set to "seeds" if psprefab is nil)
-        inst.components.periodicspawner:SetDensityInRange(60, 2)
-        inst.components.periodicspawner:SetMinimumSpacing(30)
-	--[[ 	inst.components.periodicspawner:SetSpawnTestFn( SeedSpawnTest ) ]]
-
-        inst:ListenForEvent("ontrapped", OnTrapped)
-
         inst:ListenForEvent("attacked", OnAttacked)
-		
-
-		local birdspawner = TheWorld.components.birdspawner
-        if birdspawner ~= nil then
-            inst:ListenForEvent("onremove", birdspawner.StopTrackingFn)
-            inst:ListenForEvent("enterlimbo", birdspawner.StopTrackingFn)
-            --inst:ListenForEvent("exitlimbo", extrabirdspawner.StartTrackingFn)
-            birdspawner:StartTracking(inst)
-        end
-
-        MakeFeedablePet(inst, TUNING.BIRD_PERISH_TIME, nil, OnDropped)
-
         return inst
     end
-    
-    return Prefab("forest/animals/"..name, fn, assets, prefabs)
+
+    return Prefab("forest/animals/" .. name, fn, assets, prefabs)
 end
 
 --TEMPLATE
 --makebird("prefabname", "soundname", {loottables}, "periodcspawnprefab", FOODTYPE.TYPE, scale, animremap)
-
-return 	makebird(
-				"phoenix",
-				"crow",
-				{ {"bluegem", 0.8}, {"feather_crow", 1.1}},
-				"bluegem",
-				FOODTYPE.ROUGHAGE,
-				1.6
-				)
+return makebird(
+    "phoenix",
+    "crow",
+    { { "bluegem", 0.8 }, { "feather_crow", 1.1 } },
+    "bluegem",
+    FOODTYPE.ROUGHAGE,
+    1.3
+)
